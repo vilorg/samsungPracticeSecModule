23b66e4a4c9ab62b63422a8676010c0ad96dabfd
vilorg <PetrovDmitryS@yandex.ru>
2021-01-16 14:29:20 +0500

    Initional commit

---
+++ app/src/main/java/com/vilorg/samsung_module_two/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vilorg.samsung_module_two;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
